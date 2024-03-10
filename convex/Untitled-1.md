95c76300a721c592a41c2f99abc622b0ccf98a49
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-03-10 03:57:20 +0100

    feat: first steps to actual file uploads on convex

---
+++ convex/Untitled-1.md
@@ -0,0 +1,2 @@
+  https://enabling-tapir-90.clerk.accounts.dev|user_2dS32Br4Ny80SLNgzPgvBSwZH1c
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,50 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignedOut, SignedIn, SignOutButton, useOrganization, useUser } from "@clerk/nextjs";
+import { useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, } from "@/components/ui/dialog";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z.custom<FileList>((val) => val instanceof FileList, "Required").refine((files) => files.length > 0 , "Required"),
+})
 export default function Home() {
+
+  // Auth related logic
   const organization = useOrganization();
   const user = useUser();
+
+  // FORM related logic
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  })
+
+  const fileRef = form.register("file");
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  // API interaction related logic
   const createFile = useMutation(api.files.createFile);
   let orgId: string | undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -14,15 +52,62 @@ export default function Home() {
   }
   // skip the query if there's no organization yet
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip');
+
+  // UI
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold"> Your Files </h1>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button onClick={() => {
+              if (!orgId) return;
+              createFile({ name: "Hello world", orgId })
+            }}>Upload File</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your File Here</DialogTitle>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder="title" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="file"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>File</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="file" {...fileRef}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Submit</Button>
+                </form>
+              </Form>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
       {files?.map((file) => {
         return <div key={file._id}> {file.name}</div>
       })}
-      <Button onClick={() => {
-        if (!orgId) return;
-        createFile({ name: "Hello world", orgId })
-      }}>Click me</Button>
     </main>
   );
 }
