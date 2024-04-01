831a3db4858f15d12e44802921f9eb51c073dd79
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-04-01 13:55:57 +0100

    feat: header links

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Header() {
   return <div className="border-b py-4 bg-gray-50">
     <div className="container mx-auto flex justify-between items-center">
-      <div className="flex items-center gap-2 text-xl">
+      <Link href="/" className="flex items-center gap-2 text-xl">
         <Image width={48} height={48} src="/logo.png" alt="File drive logo" />
         FileDrive
-      </div>
+      </Link>
+      <Button variant={'outline'}>
+        <Link href="/dashboard/files">Your Files</Link>
+      </Button>
       <div className="flex gap-2">
         <SignedOut>
           <SignInButton></SignInButton>
